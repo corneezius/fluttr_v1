b96f7cb5d7f4d940fcc6872c58a083f544beff78
Lisa MacCarrigan and Sam Peppard <sam@>
2016-08-31 23:26:49 -0400

    add modal comment logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 // Business logic
 
-function Idea(parameterNameInput, parameterIdeaDescription) {
-  this.nameInput = parameterNameInput;
-  this.ideaDesc = parameterIdeaDescription;
-  this.comments = ["comment 1", "comment2"];
+function Idea(name, idea) {
+  this.nameInput = name;
+  this.ideaDesc = idea;
+  this.comments = [];
   this.timeStamp = new Date();
   this.ideaId = 1;
 }
 var ideaDatabase = [];
-// Idea.prototype.ideaBox = function() {
-//   this.
-// }
 
 //Front-End Logic:
 
 $(document).ready(function () {
+
+  var currentID;
 
   $("form#user-idea").submit(function(event) {
     event.preventDefault();
@@ -23,35 +22,44 @@ $(document).ready(function () {
     }
     else {
       var userName = $("input#enter-name").val();
-    };
+    }
 
     var userIdea = $("textarea#idea-description").val();
 
     var newIdeaEntered = new Idea(userName, userIdea);
-
 
     ideaDatabase.push(newIdeaEntered);
 
     var uniqueId = ideaDatabase.length;
     newIdeaEntered.ideaId = uniqueId;
 
-
     $(".idea-section").prepend('<div class="col-xs-12 col-sm-6 col-md-4 idea-box">' +
                               '<p class="new-idea">' + newIdeaEntered.ideaDesc + '</p>' +
                               '<p class="creator-name"> via ' + newIdeaEntered.nameInput + '</p>' +
-                              '<button id="'+ newIdeaEntered.ideaId + '" class="btn btn-secondary btn-sm discussion" data-toggle="modal" data-target="#ideaModal">More</button>' +
+                              '<button id="'+ newIdeaEntered.ideaId + '" class="btn btn-secondary btn-sm discussion" data-toggle="modal" data-target="#idea-modal">More</button>' +
                               '</div>');
-$(".discussion").first().click(function() {
-  var thisButtonId = $(this).attr('id');
-  $("#ideaModal .modal-header").text(ideaDatabase[thisButtonId - 1].ideaDesc);
-  ideaDatabase[thisButtonId - 1].comments.forEach(function(currentComment){
-    $("#ideaModal .modal-body").prepend("<p class='visible-comment'>" + currentComment + "</p>");
-  })
 
-});
 
-  $("input#enter-name").val("");
-  $("textarea#idea-description").val("");
+    $(".discussion").first().click(function() {
+      $(".visible-comment").text("");
+      var thisButtonId = $(this).attr('id');
+      currentID = thisButtonId;
+      $("#idea-modal .modal-header").text(ideaDatabase[thisButtonId - 1].ideaDesc);
+      ideaDatabase[thisButtonId - 1].comments.forEach(function(currentComment) {
+        $("#idea-modal .modal-body").prepend("<p class='visible-comment'>" + currentComment + "</p>");
+      });
+    });
+
+    $(".submit-comment").first().click(function() {
+      var newComment = $(".input-comment").val();
+      $("#idea-modal .modal-body").prepend("<p class='visible-comment'>" + newComment + "</p>");
+      ideaDatabase[currentID - 1].comments.push(newComment);
+      $(".input-comment").val("");
+    });
+
+    $("input#enter-name").val("");
+    $("textarea#idea-description").val("");
 
   });
+
 });
