70699425bf86a409e545c139603c237934072c94
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-09-01 12:34:48 -0400

    remove extra JS that prepends ideas to idea section, add styling to title statement, change colors in color array

--- js/scripts.js
+++ js/scripts.js
@@ -45,46 +45,38 @@ $("#anonymous-check").click(function() {
 
     if (userIdea === "") {
       alert("Please enter your idea in the field and try again!")
-    } else {
-      $(".idea-section").prepend('<div class="col-xs-12 col-sm-6 col-md-4 idea-box">' +
-      '<p class="new-idea">' + newIdeaEntered.ideaDesc + '</p>' +
-      '<p class="creator-name"> via ' + newIdeaEntered.nameInput + '</p>' +
-      '<button id="'+ newIdeaEntered.ideaId + '" class="btn btn-secondary btn-sm discussion" data-toggle="modal" data-target="#idea-modal">More</button>' +
-      '</div>');
-    };
+    }
+    else {
 
+      var bgColorArray = ['#FF6400', '#E138AA', '#00b398', '#B3001B', '#A54797', '#26A9E0'];
+      selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 
+      $(".idea-section").prepend('<div style="background-color: ' + selectBG + ';" class="col-xs-12 col-sm-6 col-md-4 idea-box">' +
+                                '<p class="new-idea">' + newIdeaEntered.ideaDesc + '</p>' +
+                                '<p class="creator-name"> via ' + newIdeaEntered.nameInput + '</p>' +
+                                '<button id="'+ newIdeaEntered.ideaId + '" class="btn btn-secondary btn-sm discussion" data-toggle="modal" data-target="#idea-modal">More</button>' +
+                                '</div>');
 
-    var bgColorArray = ['#FF6400','#FFD740','#E138AA', '#34D1B2'];
-    selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
-
-    $(".idea-section").prepend('<div style="background-color: ' + selectBG + ';" class="col-xs-12 col-sm-6 col-md-4 idea-box">' +
-                              '<p class="new-idea">' + newIdeaEntered.ideaDesc + '</p>' +
-                              '<p class="creator-name"> via ' + newIdeaEntered.nameInput + '</p>' +
-                              '<button id="'+ newIdeaEntered.ideaId + '" class="btn btn-secondary btn-sm discussion" data-toggle="modal" data-target="#idea-modal">More</button>' +
-                              '</div>');
-
-
-    $(".discussion").first().click(function() {
-      $(".visible-comment").text("");
-      var thisButtonId = $(this).attr('id');
-      currentID = thisButtonId;
-      $("#idea-modal .modal-header").text(ideaDatabase[thisButtonId - 1].ideaDesc);
-      ideaDatabase[thisButtonId - 1].comments.forEach(function(currentComment) {
-        $("#idea-modal .modal-body").prepend("<p class='visible-comment'>" + currentComment + "</p>");
+      $(".discussion").first().click(function() {
+        $(".visible-comment").text("");
+        var thisButtonId = $(this).attr('id');
+        currentID = thisButtonId;
+        $("#idea-modal .modal-header").text(ideaDatabase[thisButtonId - 1].ideaDesc);
+        ideaDatabase[thisButtonId - 1].comments.forEach(function(currentComment) {
+          $("#idea-modal .modal-body").prepend("<p class='visible-comment'>" + currentComment + "</p>");
+        });
       });
-    });
 
-    $(".submit-comment").first().click(function() {
-      var newComment = $(".input-comment").val();
-      $("#idea-modal .modal-body").prepend("<p class='visible-comment'>" + newComment + "</p>");
-      ideaDatabase[currentID - 1].comments.push(newComment);
-      $(".input-comment").val("");
-    });
+      $(".submit-comment").first().click(function() {
+        var newComment = $(".input-comment").val();
+        $("#idea-modal .modal-body").prepend("<p class='visible-comment'>" + newComment + "</p>");
+        ideaDatabase[currentID - 1].comments.push(newComment);
+        $(".input-comment").val("");
+      });
 
-    $("input#enter-name").val("");
-    $("textarea#idea-description").val("");
-
+      $("input#enter-name").val("");
+      $("textarea#idea-description").val("");
+    }
   });
 
 });
